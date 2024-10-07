8dd5b1d95b3cc62f224d4fd101210194425559ce
Wellington <welltecnic@hotmail.com>
2024-10-07 09:09:16 -0300

    Criando o Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,10 @@
+import {Link} from 'react-router-dom'
+
 const Nav =()=>{
     return (
         <>
-        <h1>Nav</h1>
+        <Link to="/">Home</Link>
+        <Link to="/produto">Produto</Link>
         </>
     )
 }
