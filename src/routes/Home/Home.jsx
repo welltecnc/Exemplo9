685a16081dba9034309762edb74e843046f33a4f
Wellington <welltecnic@hotmail.com>
2024-10-07 09:50:11 -0300

    criando o cadastro e mostra na home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react"
+
 const Home =()=>{
+
+    const [produtos, setProdutos]= useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/produtos")
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            setProdutos(data)
+        })
+    },[])
+
     return(
         <>
-        <h1>Home</h1>
+            {produtos.map((item, index)=>(
+                <div key={index}>
+                    <p>{item.nome}</p>
+                    <img src={item.foto}/>
+                </div>
+            ))}
         </>
     )
 }
--- src/routes/Produto/Produto.jsx
+++ src/routes/Produto/Produto.jsx
@@ -1,8 +1,76 @@
+import { useState, useEffect } from "react"
+import {useNavigate, useParams} from 'react-router-dom'
+
 const Produto=()=>{
+
+    let{id} = useParams();
+
+    const navigate = useNavigate();
+
+    const [produtos, setProdutos]=useState({
+        id,
+        nome:"",
+        foto:""
+    })
+
+    const handleChange=(e)=>{
+        setProdutos({...produtos,[e.target.name]:e.target.value})};
+
+    const handleSubmit =(e)=>{
+    e.preventDefault();
+    fetch(`http://localhost:5000/produtos/${id ? id :""}`,{
+        method:"post",
+        headers:{
+            'Content-Type': "application/json"
+        },
+        body:JSON.stringify(produtos)
+    }).then(()=>{
+        navigate("/")
+    })      
+    }
+
+    useEffect(()=>{
+        if(id){
+            fetch(`http://localhost:5000/produtos/${id}`)
+            .then((res)=>{
+                return res.json();
+            })
+            .then((data)=>{
+                setProdutos(data)
+            })
+        }
+    },[id])
+
+
     return(
-        <>
-        <h1>Produto</h1>
-        </>
+        <section className="produto">
+            <h1>Cadastro de Produto</h1>
+            <form onSubmit={handleSubmit}>
+                <p>
+                    <label htmlFor="nome">Nome do Produto</label>
+                    <input
+                        type="text"
+                        name="nome"
+                        id="nome"
+                        value={produtos.nome}
+                        onChange={handleChange}
+                    />
+                </p>
+                <p>
+                    <label htmlFor="foto">Imagem do Produto</label>
+                    <input
+                        type="url"
+                        name="foto"
+                        id="foto"
+                        value={produtos.foto}
+                        onChange={handleChange}
+                    />
+                </p>
+                <button type="submit">Cadastrar</button>
+
+            </form>
+     
+        </section>
     )
 }
 export default Produto
